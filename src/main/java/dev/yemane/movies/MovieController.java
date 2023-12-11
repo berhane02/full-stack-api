19a592ada6c9e2f636e95bd2ece0cd38c363c15d
berhane02 <yberhane1@outlook.com>
2023-12-10 23:15:49 -0500

    add posting db

--- src/main/java/dev/yemane/movies/MovieController.java
+++ src/main/java/dev/yemane/movies/MovieController.java
@@ -1,13 +1,16 @@
 package dev.yemane.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/movies")
@@ -19,7 +22,10 @@ import java.util.List;
          return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
      }
 
-     @GetMapping("/{id}")
-    public ResponseEntity<Movie> getSingleMove
+     @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMove(@PathVariable String  imdbId) {
+        return  new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+     }
+
 }
 
